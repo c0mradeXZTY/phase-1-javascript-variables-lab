645c5e1ef0b1c8fcf6bf39c386aaa5103d584232
c0mradeXZTY <mavedimavela@gmail.com>
2023-12-18 00:24:29 -0800

    Done working with variables

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution in this file!
+const companyName = 'Scuber';
+let mostProfitableNeighborhood = 'Chelsea';
+mostProfitableNeighborhood = 'Chelsea'
+
+let companyCeo ='Susan_Smith';
+companyCeo = 'Susan Smith';
+
+
+
